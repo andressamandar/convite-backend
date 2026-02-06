249650828a4550c74d8deb5b0e132da33f536481
Andressa Morais <andressa-am@hotmail.com>
2026-02-06 16:53:01 -0300

    Backend RSVP MongoDB

---
+++ server.js
@@ -0,0 +1,57 @@
+
+const express = require("express");
+require("dotenv").config();
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+
+const uri = process.env.MONGO_URI;
+const client = new MongoClient(uri);
+
+let collection;
+
+async function iniciar() {
+  try {
+    await client.connect();
+    console.log("✅ Conectado ao MongoDB");
+
+    // 🔥 AQUI A CORREÇÃO
+    const db = client.db("convite");
+    collection = db.collection("aniversario_agnes");
+
+
+    app.listen(3000, () => {
+      console.log("🚀 Servidor rodando na porta 3000");
+    });
+
+  } catch (err) {
+    console.error("Erro ao conectar:", err);
+  }
+}
+
+iniciar();
+
+app.post("/rsvp", async (req, res) => {
+  try {
+    if (!req.body.nome) {
+      return res.status(400).send("Nome não enviado");
+    }
+
+    await collection.insertOne({
+      nome: req.body.nome,
+      data_confirmacao: new Date()
+    });
+
+    console.log("🎉 RSVP salvo:", req.body.nome);
+    res.send("Salvo com sucesso");
+
+  } catch (err) {
+    console.error("❌ Erro ao salvar:", err);
+    res.status(500).send("Erro no servidor");
+  }
+});
